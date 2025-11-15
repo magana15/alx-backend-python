7455a48e54ad2e71efae749ea55d7457fb047c82
joel magana <joelmagana15@gmail.com>
2025-11-15 13:05:28 +0300

    test file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,13 +2,15 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-"""A class to test the nested map"""
+
 class TestAccessNestedMap(unittest.TestCase):
+    """Tests for utils.access_nested_map"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-	"""Returns the expected value for valid paths"""
+        """Returns expected value for valid paths."""
         self.assertEqual(access_nested_map(nested_map, path), expected)

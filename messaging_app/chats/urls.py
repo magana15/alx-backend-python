c787494ec872ff475cf1d6cdbbac27f3b7d73636
joel magana <joelmagana15@gmail.com>
2025-11-22 14:31:55 +0300

    ckecker reqs

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 router = routers.DefaultRouter()
 

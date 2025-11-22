8d40cd43b6495a847fa0ffecdb49708d4ee52857
joel magana <joelmagana15@gmail.com>
2025-11-22 14:23:50 +0300

    checker reqs

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+router = routers.DefaultRouter()
 
 router = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-
+from rest_framework import filters
 from .models import Conversation, Message
 from .serializers import (
     ConversationListSerializer,

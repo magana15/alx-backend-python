ac667c1b14fe15d352692e6411949444b0b16513
joel magana <joelmagana15@gmail.com>
2025-11-29 12:57:03 +0300

    Notification

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,26 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from django.db import transaction
-from .models import Message, MessageHistory 
+from .models import Message, MessageHistory, Notification
+
+@receiver(post_save, sender=Message)
+def create_notification_on_message(sender, instance: Message, created, **kwargs):
+    """
+    When a new Message is created, create a Notification for the receiver.
+    Use transaction.on_commit to avoid creating notifications for rolled-back transactions.
+    """
+    if not created:
+        return
+
+    # ensure message save transaction has committed before creating the notification
+    def _create_notification():
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance,
+            verb="sent you a message"
+        )
+    transaction.on_commit(_create_notification)
 
 @receiver(pre_save, sender=Message)
 def log_message_old_content(sender, instance: Message, **kwargs):
